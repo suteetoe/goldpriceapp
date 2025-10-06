6fa5c89b90da23a5648a2af1710e2b3990b5dd19
suteetoe <sutee.toe@gmail.com>
2025-10-06 18:02:05 +0700

    complete iamge slidshow

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -52,14 +52,17 @@ class PriceBoardPage extends StatelessWidget {
                         ? Row(
                           children: const [
                             SizedBox(width: 420, child: LeftPanel()),
-                            Expanded(child: RightPhoto()),
+                            Expanded(child: RightSlideshow()),
                           ],
                         )
                         : const Column(
                           children: [
-                            SizedBox(height: 560, child: RightPhoto()),
-                            SizedBox(height: 12),
-                            LeftPanel(),
+                            const SizedBox(
+                              height: 560,
+                              child: RightSlideshow(),
+                            ),
+                            const SizedBox(height: 12),
+                            const LeftPanel(),
                           ],
                         ),
               ),
@@ -165,7 +168,7 @@ class _BrandHeader extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                'GOLDSIGN',
+                'GOLEPRICE',
                 style: GoogleFonts.montserrat(
                   color: gold,
                   fontSize: 34,
@@ -174,7 +177,7 @@ class _BrandHeader extends StatelessWidget {
                 ),
               ),
               Text(
-                'ห้างเพชรทองโกลด์ไซน์',
+                'ห้างทองทดสอบ',
                 style: GoogleFonts.notoSansThai(
                   color: Colors.white,
                   fontSize: 16,
@@ -243,29 +246,89 @@ class PriceRow extends StatelessWidget {
   }
 }
 
-class RightPhoto extends StatelessWidget {
-  const RightPhoto({super.key});
+class RightSlideshow extends StatefulWidget {
+  const RightSlideshow({super.key});
+
+  @override
+  State<RightSlideshow> createState() => _RightSlideshowState();
+}
+
+class _RightSlideshowState extends State<RightSlideshow> {
+  final _controller = PageController();
+  final List<String> _images = const [
+    'assets/1.webp',
+    'assets/2.webp',
+    'assets/3.webp',
+  ];
+  int _index = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 3), (_) {
+      if (!mounted) return;
+      _index = (_index + 1) % _images.length;
+      _controller.animateToPage(
+        _index,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Replace with an asset image by adding it to pubspec.yaml as shown below.
     return Container(
       color: Colors.white,
       child: Stack(
         fit: StackFit.expand,
         children: [
-          // Use AssetImage('assets/model.jpg') after adding asset.
-          Image.asset(
-            'assets/1.webp',
-            fit: BoxFit.cover,
-            errorBuilder: (c, e, s) {
-              return const Center(
-                child: Text(
-                  'Place your photo at assets/model.jpg',
-                  textAlign: TextAlign.center,
-                ),
+          PageView.builder(
+            controller: _controller,
+            itemCount: _images.length,
+            onPageChanged: (i) => setState(() => _index = i),
+            itemBuilder: (context, i) {
+              return Image.asset(
+                _images[i],
+                fit: BoxFit.cover,
+                errorBuilder:
+                    (c, e, s) => const Center(
+                      child: Text(
+                        'Add images to assets: slide1.jpg, slide2.jpg, slide3.jpg',
+                      ),
+                    ),
               );
             },
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 12,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(_images.length, (i) {
+                final active = i == _index;
+                return AnimatedContainer(
+                  duration: const Duration(milliseconds: 250),
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  height: 8,
+                  width: active ? 22 : 8,
+                  decoration: BoxDecoration(
+                    color: active ? Colors.black87 : Colors.black45,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                );
+              }),
+            ),
           ),
           Positioned(
             left: 8,
@@ -276,9 +339,9 @@ class RightPhoto extends StatelessWidget {
                 color: Colors.black.withOpacity(0.55),
                 borderRadius: BorderRadius.circular(8),
               ),
-              child: Text(
-                'GOLDSIGN',
-                style: GoogleFonts.montserrat(
+              child: const Text(
+                'GOLEPRICE',
+                style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w700,
                   letterSpacing: 1.2,
@@ -288,123 +351,6 @@ class RightPhoto extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
